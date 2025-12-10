94af72fa1cd07397a2fa7fd4d29ca9adb08a3f28
sonal-aot <sonal.sai@aot-technologies.com>
2025-12-10 16:26:14 +0530

    feat: implement SendDialog, refine file sending flow, add connection loading states, and introduce transfer state reset

--- client/src/shared/components/index.js
+++ client/src/shared/components/index.js
@@ -5,3 +5,4 @@ export { default as ConnectionArea } from "./ConnectionArea/ConnectionArea";
 export { default as ConnectionDialog } from "./ConnectionDialog/ConnectionDialog";
 export { default as WaitingDialog } from "./WaitingDialog/WaitingDialog";
 export { default as ReceiveDialog } from "./ReceiveDialog/ReceiveDialog";
+export { default as SendDialog } from "./SendDialog/SendDialog";
